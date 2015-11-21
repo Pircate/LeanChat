12a3eb3c5c843bbab993222f3666c5e478944d84
removeAllBug <gao497868860@163.com>
2015-11-22 00:02:12 +0800

    无
    无

---
+++ Vender/SCNavigation/SCShared.h
@@ -0,0 +1,23 @@
+//
+//  SCShared.h
+//
+//  Created by Singro on 5/19/15.
+//  Copyright (c) 2014 Singro. All rights reserved.
+//
+
+
+#import "FrameAccessor.h"
+
+//#define kNavigationBarTintColor [UIColor blackColor]
+//#define kNavigationBarColor     [UIColor colorWithWhite:1.00 alpha:0.980]
+//#define kNavigationBarLineColor [UIColor colorWithWhite:0.869 alpha:1]
+
+#define kNavigationBarTintColor [UIColor whiteColor]
+// 设置导航背景颜色
+#define kNavigationBarColor     [UIColor colorWithRed:0.9255 green:0.3294 blue:0.2863 alpha:1]
+
+// 设置导航栏底边线颜色
+#define kNavigationBarLineColor [UIColor lightGrayColor]
+
+// 设置 title 字体大小
+#define kNavigationBarTintFont [UIFont systemFontOfSize:17]
